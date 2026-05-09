982fe9269818fac0301934a49424a787027568d5
thiveth <denselokah12@gmail.com>
2026-05-08 23:23:15 -0400

    Added comments for clarity to main.c

--- main.c
+++ main.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
- #include <string.h>
+#include <string.h>
 
 double add (double x, double y){
-
     return x + y;
-
 }
 
 double sub(double x, double y){
     return x - y;
-
 }
 
 double mult(double x, double y){
@@ -28,19 +25,19 @@ int main() {
     double x, y;
 
     while (1){
-        int isValid = 1;
+        int isValid = 1; 
         char buffer[3];
         printf("\nEnter choice (add [1], subtract [2], multiply[3], divide[4]): ");
         fgets(buffer, sizeof(buffer), stdin);
         
-        if (strchr(buffer, '\n') == NULL){
+        if (strchr(buffer, '\n') == NULL){ // Boolean checks to see if there's no newline char in buffer[]
             int c;
             while ((c = getchar()) != '\n' && c != EOF);  
         }
-
-        if (buffer[0] == '\n'){
+    
+        if (buffer[0] == '\n'){ // Checks to see if enter is hit in terminal and newline is returned by fgets()
             printf("Invalid choice try again.\n");
-            continue;
+            continue; 
         }
 
         for (size_t i = 0; i < (sizeof(buffer) / sizeof(buffer[0])); i++){
@@ -48,17 +45,16 @@ int main() {
                 isValid = 0;
 
                 printf("Invalid choice try again.\n");
-                break;
+                break; 
             }
         }
 
-        if (isValid == 0) continue;
+        if (isValid == 0) continue; // After for loop above is terminated next iteration of while loop starts
 
-        if (isValid == 1) choice = atoi(buffer);
+        if (isValid == 1) choice = atoi(buffer); // If no errors in input then the input is converted into an integer and goes into next stage
     
 
-
-
+        
         if (choice == 0){
             printf("Exiting..\n");
             break;
@@ -66,8 +62,8 @@ int main() {
         else if(choice == 1){
             printf("\nEnter first and second number with a space between both: ");
             scanf("%lf %lf", &x, &y);
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF); 
+            int c; 
+            while ((c = getchar()) != '\n' && c != EOF); // Flushes stdin buffer
             printf("%.2f\n", add(x, y));
         }  
         else if(choice == 2){
@@ -89,6 +85,7 @@ int main() {
             scanf("%lf %lf", &x, &y);
             
 
+            // Catches division by 0
             if (y == 0){
                 printf("Undefined\n");
             }
